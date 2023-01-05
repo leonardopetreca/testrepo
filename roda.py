cf95dc8a722694a157419879ba2b6cc0e6a0cf13
leonardopetreca <61334273+leonardopetreca@users.noreply.github.com>
2023-01-05 15:59:10 -0300

    Add files via upload

---
+++ roda.py
@@ -0,0 +1,15 @@
+import subprocess 
+import pandas as pd
+import os
+import testefunc as aq
+
+aq.func()
+
+#file = str("C:\\Users\\GNPUO\\OneDrive - Bayer\\Desktop\\automatizações\\aderencia"+ "\\testefunc.py")
+#file2 = aq.os.getcwd() +"\\testefunc.py"
+#subprocess.run(["python", file2 ])
+#aq.mandaOi()
+
+#print(aq.os.getcwd())
+#print(err)
+#print(out)
